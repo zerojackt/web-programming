d87cc35fe33599fd6d09f56cb6457651add9935a
zerojack <52700377+zerojackt@users.noreply.github.com>
2019-09-18 19:12:49 +0900

    js learn variable and more

---
+++ varialbe.js
@@ -0,0 +1,16 @@
+var a = 2;
+var b = "aaa";
+
+const hello = "Hello";
+const world = hello || "World";
+console.log(world);
+
+const data = 11;
+const result = (data > 10) ? "ok" : "fail";
+
+console.log(result);
+
+
+console.log(a===b);
+
+console.log(toString.call(a));
