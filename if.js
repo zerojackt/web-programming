33baceb2d6d9036bec576f6c8cf13789ba9b5597
zerojack <52700377+zerojackt@users.noreply.github.com>
2019-09-20 21:15:16 +0900

    if and loop

---
+++ if.js
@@ -0,0 +1,32 @@
+var a = 1;
+if(true) {
+  console.log(a);
+  
+} else {
+
+}
+
+switch (a) {
+    case a: {
+
+    }
+    case b: {
+
+    }
+}
+
+for (var i = 1;i<10;i++) {
+    console.log("1"+"*"+i+"="+a*i);
+}
+
+var array = [1,2,3];
+for(var i =0, len=array.length; i<len; i++) {
+
+}
+
+console.log(typeof "a")
+console.log(typeof 'a')
+
+var result = "a,b".replace(",",":");
+console.log(result)
+
