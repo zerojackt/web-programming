6bc05a460ea04d4ae19bca5343c81cbee7b8ee6b
zerojack <52700377+zerojackt@users.noreply.github.com>
2019-10-04 21:55:29 +0900

    jsp & servlet

---
+++ LogicServlet.java
@@ -0,0 +1,47 @@
+package examples;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogicServlet
+ */
+@WebServlet("/logic")
+public class LogicServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogicServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int v1 = (int) (Math.random()*100 + 1);
+		int v2 = (int) (Math.random()*100 + 1);
+		int result = v1 + v2;
+		
+		request.setAttribute("v1", v1);
+		request.setAttribute("v2", v2);
+		request.setAttribute("result", result);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/result.jsp");
+		requestDispatcher.forward(request, response);
+		
+		
+	}
+
+}
---
+++ result.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+${v1 } + ${v2 } = ${result }
+
+<%-- <%
+int v1 = (Integer) request.getAttribute("v1");
+int v2 = (Integer) request.getAttribute("v2");
+int result = (Integer) request.getAttribute("result");
+
+<%="v1 : "+v1 %> <br>
+<%="v2 : "+v2 %> <br>
+<%=v1 %> + <%=v2 %> = <%=result %> --%>
+
+</body>
+</html>
